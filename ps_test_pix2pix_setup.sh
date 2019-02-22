37e6581636517711a0ca01696f8f2e1f66637a6c
Owen Hua <owen.hua@mp-owenh-ml.local>
2019-02-22 10:34:51 -0800

    datset_num option fix

--- ps_test_pix2pix_setup.sh
+++ ps_test_pix2pix_setup.sh
@@ -2,4 +2,4 @@ pip3 install https://download.pytorch.org/whl/cu100/torch-1.0.1.post2-cp35-cp35m
 python test.py --dataroot /dataset/holopix --name holopix-pix2pix-1k-batch-3-date-02-21-002 \
 --model pix2pix --direction AtoB --gpu_ids 3 --batch_size 1 \
 --checkpoints_dir /persistent --output_nc 1 --dataset_mode holopix \
---preprocess resize --load_size 256 --load_iter 200
+--dataset_num 1000 --preprocess resize --load_size 256 --load_iter 200
